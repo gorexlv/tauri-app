20aa88ec1853c902ab3a18a96fa9ed7422ddfe9c
Chao Lv <gorexlv@gmail.com>
2022-07-05 23:00:22 +0800

    fixbug: resolve dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,18 +5,15 @@
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-  format!("Hello, {}!", name)
+   format!("Hello, {}!", name)
 }
 
 fn main() {
   let context = tauri::generate_context!();
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
-    .menu(if cfg!(target_os = "macos") {
-      tauri::Menu::os_default(&context.package_info().name)
-    } else {
-      tauri::Menu::default()
-    })
     .run(context)
     .expect("error while running tauri application");
 }
+
+
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,7 +1,11 @@
-import { invoke } from '@tauri-apps/api'
-import './style.css'
+import { invoke } from '@tauri-apps/api';
+import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+// const app = document.querySelector<HTMLDivElement>('#app')!
+// app.innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
 
 // now we can call our Command!
 // Right-click the application background and open the developer tools.
@@ -9,8 +13,3 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 invoke('greet', { name: 'World' })
   // `invoke` returns a Promise
   .then((response) => console.log(response))
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
